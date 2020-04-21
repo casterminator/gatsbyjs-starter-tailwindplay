bebb2ae69441fc711407895f8fc0c6e99869c1a5
casterminator <44626745+casterminator@users.noreply.github.com>
2020-04-21 10:51:33 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -109,7 +109,7 @@ function Layout({ children }) {
             {/* Alternate features */}
             <section className="bg-white border-b py-8">
               <div className="container max-w-5xl mx-auto m-8">
-                <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">{data.site.siteMetadata.title}</h2>
+                <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">THE FASTEST PATH TO BETTER TRAINING</h2>
                 <div className="w-full mb-4">	
                   <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
                 </div>
