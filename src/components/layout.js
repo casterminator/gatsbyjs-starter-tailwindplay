b97232ba5c574eed8acf3b778251b352790ae0b5
casterminator <44626745+casterminator@users.noreply.github.com>
2020-04-21 12:20:48 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -287,7 +287,24 @@ function Layout({ children }) {
                   </div>
                 </div>
                 
-                
+ 
+                  <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                  <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+                    <a href="#" className="flex flex-wrap no-underline hover:no-underline">
+                      <p className="w-full text-gray-600 text-xs md:text-sm px-6">Open-Source</p>
+                      <div className="w-full font-bold text-xl text-gray-800 px-6">Coded by <a href="https://twitter.com/Sm0keDev">Sm0ke</a> </div>
+                      <p className="text-gray-800 text-base px-6 mb-5">
+                        The source code is released under a license in which the copyright holder grants users the rights to study, change, and distribute the software to anyone and for any purpose.
+                      </p>
+                    </a>
+                  </div>
+                  <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+                    <div className="flex items-center justify-end">
+                      <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Action</button>
+                    </div>
+                  </div>
+                </div>
+  
               </div>
 
             </section>
